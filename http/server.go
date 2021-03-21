98d5b66909d5b982b7ad2f9c3dd0fe1cac595559
Jack Caldwell <jackcaldwell@ekm.com>
2021-03-21 16:43:36 +0000

    ne wstuff

--- http/server.go
+++ http/server.go
@@ -49,7 +49,7 @@ func (s *Server) UseTLS() bool {
 }
 
 func healthCheck(w http.ResponseWriter, _ *http.Request) {
-	_, _ = w.Write([]byte("Healthy original"))
+	_, _ = w.Write([]byte("Healthy new"))
 }
 
 func (s *Server) Open() (err error) {
